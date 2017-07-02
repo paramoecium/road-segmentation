ca8d93f82a85a04b939ae5043dad784d2aaec6b7
Yuchen Tsai <ytsai@ethz.ch>
2017-07-02 15:17:46 +0200

    remove mask_to_submission_strings

--- src/mask_to_submission.py
+++ src/mask_to_submission.py
@@ -14,25 +14,19 @@ def patch_to_label(patch):
     else:
         return 0
 
-
-def mask_to_submission_strings(image_filename):
-    """Reads a single image and outputs the strings that should go into the submission file"""
-    img_number = int(re.search(r"\d+", image_filename).group(0))
-    im = mpimg.imread(image_filename)
-    patch_size = 16
-    for j in range(0, im.shape[1], patch_size):
-        for i in range(0, im.shape[0], patch_size):
-            patch = im[i:i + patch_size, j:j + patch_size]
-            label = patch_to_label(patch)
-            yield("{:03d}_{}_{},{}".format(img_number, j, i, label))
-
-
 def masks_to_submission(submission_filename, *image_filenames):
     """Converts images into a submission file"""
     with open(submission_filename, 'w') as f:
         f.write('id,prediction\n')
-        for fn in image_filenames[0:]:
-            f.writelines('{}\n'.format(s) for s in mask_to_submission_strings(fn))
+        for filename in image_filenames[0:]:
+            img_number = int(re.search(r"\d+", filename).group(0))
+            im = mpimg.imread(filename)
+            patch_size = 16
+            for j in range(0, im.shape[1], patch_size):
+                for i in range(0, im.shape[0], patch_size):
+                    patch = im[i:i + patch_size, j:j + patch_size]
+                    label = patch_to_label(patch)
+                    f.writelines("{:03d}_{}_{},{}\n".format(img_number, j, i, label))
 
 
 if __name__ == '__main__':

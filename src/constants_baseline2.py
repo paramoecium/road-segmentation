bed44030764f7b000f20deec3538373912b7f956
samk <skezle@users.noreply.github.com>
2017-07-03 11:30:49 +0200

    Baseline2 submission

---
+++ src/constants_baseline2.py
@@ -0,0 +1,28 @@
+# CONSTANTS THAT ARE COMMON TO MULTIPLE SOURCE FILES
+
+# Number of input and test images
+TRAINING_SIZE = 100
+TEST_SIZE = 50
+
+# Dimensions of input images
+IMG_WIDTH = 200
+IMG_HEIGHT = 200
+
+# Parameters related to the CNN architecture
+IMG_PATCH_SIZE = 8
+IMG_CONTEXT_SIZE = 32
+IMG_BORDER_SIZE = int((IMG_CONTEXT_SIZE - IMG_PATCH_SIZE) / 2)
+IMG_PATCH_STRIDE = 4
+
+# File paths
+OBJECTS_PATH = "../objects/"
+PATCHES_MEAN_PATH = OBJECTS_PATH + "patches_mean"
+RESULTS_PATH = "../results"
+
+# Post-processing
+DICT_PATCH_SIZE = (5, 5)
+LOW_RANK_TARGET = 3  # desired rank of output
+
+POSTPRO_PATCH_SIZE = 16  # patch size we need to predict in the end
+POSTPRO_SVM_PATCH_SIZE = 7  # use 7 to get best score with SVM
+POSTPRO_CNN_PATCH_SIZE = 9  # use 7 to get best score with SVM
